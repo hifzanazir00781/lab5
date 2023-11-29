128eb832bc9bcb7528ccc5ba49fdeb83a3d66563
hifza <hifzanazir456@gmail.com>
2023-11-29 22:07:22 +0500

    min function by comparing three values

--- colorUtils.c
+++ colorUtils.c
@@ -11,6 +11,11 @@ int max(int x, int y, int z) {
   return m;
 }
 
+int min(int x,int y,int z){
+  int m = x < y ? x : y;
+  m = m < z ? m : z;
+}
+
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
